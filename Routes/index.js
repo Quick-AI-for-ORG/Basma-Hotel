2ad00fb3f78fd4b822455e6367457e505dbababe
Moshir Ashraf <moshir2107190@miuegypt.edu.eg>
2023-10-17 23:58:41 +0300

    exit in login

--- Routes/index.js
+++ Routes/index.js
@@ -3,7 +3,7 @@ const router=  express.Router();
 const pages = require('../Controllers/Pages')
 
 router.get('/', pages.root.basma)
-router.get('/about us', pages.root.about)
+router.get('/aboutus', pages.root.about)
 router.get('/facilities',pages.root.facilities)
 router.get('/privacyPolicy',pages.root.privacy)
 
