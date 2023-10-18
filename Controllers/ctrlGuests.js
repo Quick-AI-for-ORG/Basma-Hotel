6590c842a90240540bba909692e16bed5c7e00df
Moshir Ashraf <moshir2107190@miuegypt.edu.eg>
2023-10-18 20:42:32 +0300

    editing

--- Controllers/ctrlGuests.js
+++ Controllers/ctrlGuests.js
@@ -36,7 +36,7 @@ const register = async (req, res) => {
     })
 
     console.log(req.body);
-    sql = `INSERT INTO guests (firstName, lastName, email, password, phoneNumber, address) VALUES ('${req.body.fname}', '${req.body.lname}', '${req.body.email}', '${req.body.password}', '${req.body.phoneNumber}', ${req.body.address})`;    // let encryptedPassword = await bcrypt.hash(req.body.password, { saltRounds: 12 })
+    sql = `INSERT INTO guests (firstName, lastName, email, password, phoneNumber, address) VALUES ('${req.body.fname}', '${req.body.lname}', '${req.body.email}', '${req.body.password}', '${req.body.phoneNumber}', '${req.body.address}')`;    // let encryptedPassword = await bcrypt.hash(req.body.password, { saltRounds: 12 })
 
 
 
