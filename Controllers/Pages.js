fd53563e5cc2d859f02f67ef610194ee743ab2b7
Dalia Raafat <daliaraafat03@gmail.com>
2023-10-18 20:17:52 +0300

    Slidshow (Frontend V1)

--- Controllers/Pages.js
+++ Controllers/Pages.js
@@ -1,26 +1,25 @@
 const login = (req, res) => {
-    res.render('login', {layout:false})
-}
+  res.render("login", { layout: false });
+};
 const signup = (req, res) => {
-    res.render('signup',{layout:false})
-}
+  res.render("signup", { layout: false });
+};
 const bookings = (req, res) => {
-    res.render('myBookings')
-}
-const basma = (req, res) =>{
-    res.render('basma')
-}
-const about = (req, res) =>{
-    res.render('aboutUs')
-}
-const facilities = (req,res) =>{
-    res.render('facilities')
-}
-const privacy = (req,res) =>{
-    res.render('privacyPolicy')
-}
-module.exports = { 
-    root:{basma,about,facilities,privacy},
-    guest:{login,signup,bookings}
-   
-}
+  res.render("myBookings");
+};
+const basma = (req, res) => {
+  res.render("basma");
+};
+const about = (req, res) => {
+  res.render("aboutUs");
+};
+const facilities = (req, res) => {
+  res.render("facilities");
+};
+const privacy = (req, res) => {
+  res.render("privacyPolicy");
+};
+module.exports = {
+  root: { basma, about, facilities, privacy },
+  guest: { login, signup, bookings },
+};
