6cc1d20c1ed818c4cbbb12157ad56d8842c3cd4b
Moshir Ashraf <moshir2107190@miuegypt.edu.eg>
2023-10-18 18:09:08 +0300

    toggle in navbar

---
+++ Public/JS/toggle.js
@@ -0,0 +1,13 @@
+let sin = document.getElementById('in');
+let sup = document.getElementById('up');
+
+sup.addEventListener('mouseover', function() {
+    sin.classList.remove('btn-dark');
+    sup.classList.add('btn-dark');
+  });
+  
+sin.addEventListener('mouseout', function() {
+    sup.classList.remove('btn-dark');
+    sin.classList.add('btn-dark');
+  });
+ 
