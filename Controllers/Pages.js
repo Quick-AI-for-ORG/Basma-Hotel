79c68f8630640aa5c9e2176c8eed1691439d7fdc
Adam Gahin <adamlo2ay@gmail.com>
2023-11-14 22:13:14 +0200

    reservations now appear in the profile

--- Controllers/Pages.js
+++ Controllers/Pages.js
@@ -1,5 +1,6 @@
 const {Room} = require('../Models/Room.js')
 const ctrlRooms = require('../Controllers/ctrlRooms')
+const ctrlReservations = require('../Controllers/ctrlReservations')
 
 const login = (req, res) => {
   res.render("login", { layout: false });
@@ -29,6 +30,7 @@ const myProfile = async (req, res) => {
   if (req.session.user === undefined) {
     res.redirect("/guest/login");
   } else {
+    console.log(req.session.user)
     await ctrlReservations.guest.getUserReservations(req,res).then((result)=>{
       res.render("myProfile", {
         layout: false,
