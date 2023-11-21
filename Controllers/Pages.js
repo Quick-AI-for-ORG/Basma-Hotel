8eff10c288e831884fe66ba415e2c7870afbc70a
Moshir Ashraf <moshir2107190@miuegypt.edu.eg>
2023-11-22 01:18:33 +0200

    room

--- Controllers/Pages.js
+++ Controllers/Pages.js
@@ -77,19 +77,9 @@ const booking = (req, res) => {
   else res.redirect('/guest/login')
 }
 
-const details = async (req, res) => {
-  let room = null
-  await ctrlRooms.public.getRoom(req,res).then((result)=>{
-    room = result
-    if(room === null) res.redirect('/room')
-    res.render('details', {  user: (req.session.user === undefined ? "" : req.session.user) ,
-    room: (room === null ? "" : room)})
-  })
-  }
-
 
 module.exports = {
-  public: { basma, about, facilities, privacy, covid,login, signup, viewRooms,viewRoom,details },
+  public: { basma, about, facilities, privacy, covid,login, signup, viewRooms,viewRoom },
   guest: { myProfile,booking, logout },
   admin: {dashboard}
 };
