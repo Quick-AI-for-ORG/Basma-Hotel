be1bda43112d90eb3a2bf712798731d5cfc37164
Moshir Ashraf <moshir2107190@miuegypt.edu.eg>
2023-12-14 23:30:51 +0200

    crud admin staff

--- Models/Admin.js
+++ Models/Admin.js
@@ -1,10 +1,109 @@
 const Staff = require('./Staff')
+const Characteristic = require('./Characteristic')
+const Room = require('./Room')
+const Reservation = require('./Reservation')
+const Guest = require('./Guest')
+const Option = require('./Option')
 class Admin extends Staff{
-  constructor(adminJSON) {
-    super(adminJSON, "Admin")
-}
-    async add(user){
-     await Admin.crudInterface.create(user,userModel,"email") 
-   }
+    constructor(adminJSON) {
+      super(adminJSON, "Admin")
+    }
+    static async get(email){
+      const user = await Staff.crudInterface.get(email,userModel,"email")
+      if(user) return new Admin(user) 
+      else return null
+    }
+    async getAllCharacteristics(){
+      return await Characteristic.getAll()
+    }
+    async getAllUsers(){
+      return await User.getAll()
+    }
+    async getAllOptions(){
+      return await Option.getAll()
+    }
+   async addRoom(roomJSON){
+    let room = new Room(roomJSON)
+    await room.create()
+    return room
+    }
+    async addOption(optionJSON){
+      let option = new Option(optionJSON)
+      await option.create()
+      return option
+    }
+    async addCharacteristic(characteristicJSON){
+      let characteristic = new Characteristic(characteristicJSON)
+      await characteristic.create()
+      return characteristic
+    }
+    async removeRoom(title){
+      await Room.remove(title)
+      return true
+    }
+    async removeOption(id){
+      await Option.remove(id)
+      return true
+    }
+    async removeCharacteristic(characteristic){
+      await Characteristic.remove(characteristic)
+      return true
+    }
+    async modifyRoom(title,roomJSON){
+      let room = await Room.get(title)
+      await room.modify(roomJSON)
+      return room
+    }
+    async modifyOption(id,optionJSON){
+      let option = await Option.get(id)
+      await option.modify(optionJSON)
+      return option
+    }
+    async modifyCharacteristic(characteristic,characteristicJSON){
+      let characteristic = await Characteristic.get(characteristic)
+      await characteristic.modify(characteristicJSON)
+      return characteristic
+    }
+    async addStaff(staffJSON){
+      let staff = new Staff(staffJSON)
+      await staff.create()
+      return staff
+  }
+
+  async removeStaff(email){
+      await Staff.remove(email)
+      return true
+  }
+  async addAdmin(adminJSON){
+    let admin = new Admin(adminJSON)
+    await admin.create()
+    return admin
+  }
+  async removeAdmin(email){
+    await Admin.remove(email)
+    return true
+  }
+  async removeGuest(email){
+    await Guest.remove(email)
+    return true
+  }
+
+  async modifyStaff(email,staffJSON){
+    let staff = await Staff.get(email)
+    await staff.modify(staffJSON)
+    return staff
+  }
+  async modifyAdmin(email,adminJSON){
+    let admin = await Admin.get(email)
+    await admin.modify(adminJSON)
+    return admin
+  }
+  async modifyGuest(email,guestJSON){
+    let guest = await Guest.get(email)
+    await guest.modify(guestJSON)
+    return guest
+  }
+
+
  }
  module.exports = Admin
--- Models/Staff.js
+++ Models/Staff.js
@@ -1,7 +1,67 @@
 const User = require('./User')
+const Reservation = require('./Reservation')
+const Room = require('./Room')
+const Guest = require('./Guest')
+const Question = require('./Question')
 class Staff extends User{
     constructor(staffJSON) {
         super(staffJSON, "Staff")
     }
+    static async get(email){
+        const user = await User.crudInterface.get(email, userModel, "email")
+        if (user) return new Staff(user)
+        else return null
+    }
+    async getAllGuests(){
+        const users = await User.getAll()
+        let guests = []
+        for(let i = 0; i < users.length; i++){
+            if(users[i].role == "Guest") guests.push(await Guest.get(users[i].email))
+        }
+        if(guests.length == 0) return null
+        else return guests
+    }
+
+    async getAllReservations(){
+        return await Reservation.getAll()
+    }
+
+    async getAllRooms(){
+        return await Room.getAll()
+    }
+
+    async searchReservationsByDate(startDate=null, endDate=null){
+        return await Reservation.searchByDate(startDate, endDate)
+    }    
+
+    async searchReservationsByGuest(email){
+        return await Reservation.getGuestReservations(email)
+    }
+
+    async searchReservationsByRoom(title){
+        return await Reservation.getRoomReservations(title)
+    }
+
+    async addReservation(reservationJSON){
+        let reservation = new Reservation(reservationJSON)
+        await reservation.create()
+        return reservation
+    }
+
+    async removeReservation(id){
+        await Reservation.remove(id)
+        return true
+    }
+
+    async addGuest(guestJSON){
+        let guest = new Guest(guestJSON)
+        await guest.create()
+        return guest
+    }
+
+    async getQuestions(){
+        return await Question.getAll()
+    }
+
 }
 module.exports = Staff
