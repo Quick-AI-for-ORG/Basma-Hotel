33d713e11ac3222a7ca7f745638048184c7915a4
Dalia Raafat <daliaraafat03@gmail.com>
2023-12-19 22:41:44 +0200

    Add/edit/save charecteristics

--- Public/JS/charecteristics.js
+++ Public/JS/charecteristics.js
@@ -4,29 +4,47 @@ function addCharacteristicsRow() {
     .getElementsByTagName("tbody")[0];
   var newRow = table.insertRow(table.rows.length);
 
-  // Create grid columns for each cell
   var cell1 = newRow.insertCell(0);
   var cell2 = newRow.insertCell(1);
   var cell3 = newRow.insertCell(2);
 
-  // Add a custom class to the grid columns
   cell1.className = "custom-col";
   cell2.className = "custom-col";
   cell3.className = "custom-col";
 
-  // Insert form controls inside the grid columns
   cell1.innerHTML =
     '<input type="text" class="form-control" placeholder="Characteristic">';
   cell2.innerHTML =
     '<input type="text" class="form-control" style="font-size: large;" placeholder="Icon">';
   cell3.innerHTML =
     '<div class="btn-group d-flex justify-content-center"" role="group">' +
-    '<button class="btn btn-primary" style="margin-right: 10px;" onclick="toggleReadOnly(this)">Edit</button>' +
+    '<button class="btn btn-primary" style="margin-right: 10px;" onclick="toggleEditMode(this)">Edit</button>' +
     '<button class="btn btn-danger" onclick="deleteCharacteristicRow(this)">Delete</button>' +
     "</div>";
 
   document.getElementById("saveButtonContainer").style.cssText =
     "display: block; margin-right: 70px;";
+}
+
+function toggleEditMode(button) {
+  var row = button.closest("tr");
+
+  var inputFields = row.querySelectorAll("input");
+  inputFields.forEach(function (input) {
+    input.readOnly = !input.readOnly;
+  });
+
+  if (button.textContent == "Edit") {
+    button.textContent = "Save";
+    button.classList.remove("btn-primary");
+    button.classList.add("btn-success");
+  } else {
+    button.textContent = "Edit";
+    button.classList.remove("btn-success");
+    button.classList.add("btn-primary");
+  }
+
+  enableSaveButton();
 }
 
 function saveCharacteristicRow() {
@@ -53,18 +71,18 @@ function saveCharacteristicRow() {
 
   if (characteristic && icon) {
     document.getElementById("errorMessage").innerHTML = "";
-    var form = document.createElement('form');
-    form.style.visibility = 'hidden';
-    form.method = 'post';
-    form.action = '/admin/addCharacteristic';
-    var input1 = document.createElement('input');
-    input1.type = 'hidden';
-    input1.name = 'characteristic';
-    input1.value = characteristic; 
+    var form = document.createElement("form");
+    form.style.visibility = "hidden";
+    form.method = "post";
+    form.action = "/admin/addCharacteristic";
+    var input1 = document.createElement("input");
+    input1.type = "hidden";
+    input1.name = "characteristic";
+    input1.value = characteristic;
     form.appendChild(input1);
-    var input2 = document.createElement('input');
-    input2.type = 'hidden';
-    input2.name = 'icon';
+    var input2 = document.createElement("input");
+    input2.type = "hidden";
+    input2.name = "icon";
     input2.value = "fa-solid fa-" + icon;
     form.appendChild(input2);
     document.body.appendChild(form);
@@ -114,14 +132,12 @@ function toggleReadOnly(button) {
     }
   }
 
-  // Toggle the read-only state for the icon input field
   var iconCell = cells[1];
   var iconInput = iconCell.querySelector("input");
   if (iconInput) {
     iconInput.readOnly = !iconInput.readOnly;
     console.log("Icon field is now read-only: " + iconInput.readOnly);
   } else {
-    // If no icon input field found, create one
     var iconInput = document.createElement("input");
     iconInput.type = "text";
     iconInput.className = "form-control";
@@ -131,7 +147,6 @@ function toggleReadOnly(button) {
     iconCell.appendChild(iconInput);
   }
 
-  // Show the "Save" button container when fields are editable
   var saveButtonContainer = document.getElementById("saveButtonContainer");
   saveButtonContainer.style.display = Array.from(cells).some((cell) => {
     var input = cell.querySelector("input");
@@ -139,4 +154,24 @@ function toggleReadOnly(button) {
   })
     ? "block"
     : "none";
+}
+function saveRoomRow() {
+  var inputFields = document.querySelectorAll("#roomTable input");
+  inputFields.forEach(function (input) {
+    input.readOnly = true;
+  });
+
+  var selectFields = document.querySelectorAll("#roomTable select");
+  selectFields.forEach(function (select) {
+    select.disabled = true;
+  });
+
+  var editButtons = document.querySelectorAll(".btn-edit");
+  editButtons.forEach(function (button) {
+    button.textContent = "Edit";
+    button.classList.remove("btn-success");
+    button.classList.add("btn-primary");
+  });
+
+  document.getElementById("saveButtonContainer").style.display = "none";
 }
